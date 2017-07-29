ff94bf934ad2a5dce4d0c67d68efdfbe887a3959
AliaksandrHaurylenka <aliaksandr.haurylenka@gmail.com>
2017-07-29 23:13:41 +0200

    del form

--- frontend/controllers/TemplatesController.php
+++ frontend/controllers/TemplatesController.php
@@ -41,44 +41,6 @@ class TemplatesController extends Controller
      *
      * @return mixed
      */
-    public function actionForm()
-    {
-        /*$model = new MyForm();
-        if (Yii::$app->request->isPost) {
-            $model->file_load = UploadedFile::getInstance($model, 'file_load');
-            if ($model->upload()) {
-                if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
-                    Yii::$app->session->setFlash('success', 'Спасибо за Ваше письмо. Мы постараемся как можно быстрее Вам ответить!');
-                    return $this->refresh();
-                } else {
-                    Yii::$app->session->setFlash('error', 'Внимание! Ваше письмо по каким-то причинам не отправлено!!!');
-                }
-            }else {
-                Yii::$app->session->setFlash('error', 'Внимание! Файл не загружен!!!');
-            }
-        }*/
-
-
-        $model = new MyForm();
-        if (Yii::$app->request->isPost) {
-            $model->file_load = UploadedFile::getInstance($model, 'file_load');
-            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
-                Yii::$app->session->setFlash('success', 'Спасибо за Ваше письмо. Мы постараемся как можно быстрее Вам ответить!');
-                return $this->refresh();
-            } else {
-                Yii::$app->session->setFlash('error', 'Внимание! Ваше письмо по каким-то причинам не отправлено!!!');
-            }
-        }
-
-        return $this->render('form', compact('model'));
-    }
-
-
-    /**
-     * Displays Form.
-     *
-     * @return mixed
-     */
     public function actionOneForm()
     {
         $model = new OneForm();
